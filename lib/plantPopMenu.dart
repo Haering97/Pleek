19a64d06a0945a3c9206e789f4e3ae1131ae024e
haering.lennard <haering.lennard@smail.th-koeln.de>
2022-03-15 21:00:24 +0100

    small add multiple PopItems

--- lib/plantPopMenu.dart
+++ lib/plantPopMenu.dart
@@ -43,131 +43,134 @@ class _changeEntryModalState extends State<changeEntryModal> {
 
   @override
   Widget build(BuildContext context) {
-    return PopupMenuButton(onSelected: (newValue){
-      if(newValue == 0){
-        showModalBottomSheet(
-            context: context,
-            builder: (BuildContext context) {
-              return Container(
-                color: Colors.green.shade700,
-                width: double.infinity,
-                child: Container(
-                  margin: EdgeInsets.fromLTRB(40, 5, 40, 50),
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        const Text(
-                          "Eintrag ändern",
-                          style: TextStyle(
-                              fontSize: 25, fontWeight: FontWeight.bold),
-                        ),
-                        TextField(
-                          //TODO alten Namen dem Widget übergeben
-                          onChanged: (value) => nameChanged = true,
-                          textAlign: TextAlign.center,
-                          controller: textController,
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                    color: Colors.greenAccent, width: 2),
+    return PopupMenuButton(
+        icon: const Icon(Icons.wysiwyg),
+        onSelected: (newValue) {
+          if (newValue == 0) {
+            showModalBottomSheet(
+                context: context,
+                builder: (BuildContext context) {
+                  return Container(
+                    color: Colors.green.shade700,
+                    width: double.infinity,
+                    child: Container(
+                      margin: EdgeInsets.fromLTRB(40, 5, 40, 50),
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            const Text(
+                              "Eintrag ändern",
+                              style: TextStyle(
+                                  fontSize: 25, fontWeight: FontWeight.bold),
+                            ),
+                            TextField(
+                              //TODO alten Namen dem Widget übergeben
+                              onChanged: (value) => nameChanged = true,
+                              textAlign: TextAlign.center,
+                              controller: textController,
+                              decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Colors.greenAccent, width: 2),
+                                  ),
+                                  hintText: widget.oldName,
+                                  hintStyle: TextStyle(
+                                      color: Color.fromRGBO(0, 0, 0, 0.8),
+                                      fontSize: 20)),
+                            ),
+                            ElevatedButton(
+                              onPressed: () => selectDateAndSave(context),
+                              child: Container(
+                                  margin:
+                                      const EdgeInsets.fromLTRB(15, 10, 15, 10),
+                                  child: const Text("Anderes Datum wählen",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(fontSize: 14))),
+                              style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.resolveWith<Color>(
+                                  (Set<MaterialState> states) {
+                                    if (states
+                                        .contains(MaterialState.pressed)) {
+                                      return Colors.greenAccent;
+                                    }
+                                    return Colors.green.shade900;
+                                  },
+                                ),
                               ),
-                              hintText: widget.oldName,
-                              hintStyle: TextStyle(
-                                  color: Color.fromRGBO(0, 0, 0, 0.8),
-                                  fontSize: 20)),
-                        ),
-                        ElevatedButton(
-                          onPressed: () => selectDateAndSave(context),
-                          child: Container(
-                              margin:
-                              const EdgeInsets.fromLTRB(15, 10, 15, 10),
-                              child: const Text("Anderes Datum wählen",
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(fontSize: 14))),
-                          style: ButtonStyle(
-                            backgroundColor:
-                            MaterialStateProperty.resolveWith<Color>(
-                                  (Set<MaterialState> states) {
-                                if (states.contains(MaterialState.pressed)) {
-                                  return Colors.greenAccent;
-                                }
-                                return Colors.green.shade900;
-                              },
                             ),
-                          ),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            widget.deletePlant();
-                            Navigator.pop(context);
-                          },
-                          child: Container(
-                              margin:
-                              const EdgeInsets.fromLTRB(15, 10, 15, 10),
-                              child: const Text("Absäbeln",
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(fontSize: 14))),
-                          style: ButtonStyle(
-                            backgroundColor:
-                            MaterialStateProperty.resolveWith<Color>(
-                                  (Set<MaterialState> states) {
-                                if (states.contains(MaterialState.pressed)) {
-                                  return Colors.greenAccent;
-                                }
-                                return Colors.green.shade900;
+                            ElevatedButton(
+                              onPressed: () {
+                                widget.deletePlant();
+                                Navigator.pop(context);
                               },
+                              child: Container(
+                                  margin:
+                                      const EdgeInsets.fromLTRB(15, 10, 15, 10),
+                                  child: const Text("Absäbeln",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(fontSize: 14))),
+                              style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.resolveWith<Color>(
+                                  (Set<MaterialState> states) {
+                                    if (states
+                                        .contains(MaterialState.pressed)) {
+                                      return Colors.greenAccent;
+                                    }
+                                    return Colors.green.shade900;
+                                  },
+                                ),
+                              ),
                             ),
-                          ),
-                        ),
-                        Container(
-                          margin: EdgeInsets.fromLTRB(0, 40, 0, 0),
-                          child: ElevatedButton(
-                            onPressed: () =>
-                                Navigator.pop(context, textController.text),
-                            child: Container(
-                              //margin: const EdgeInsets.fromLTRB(15, 0, 15, 0),
-                                child: const Text("Speichern",
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(fontSize: 14))),
-                            style: ButtonStyle(
-                              backgroundColor:
-                              MaterialStateProperty.resolveWith<Color>(
+                            Container(
+                              margin: EdgeInsets.fromLTRB(0, 40, 0, 0),
+                              child: ElevatedButton(
+                                onPressed: () =>
+                                    Navigator.pop(context, textController.text),
+                                child: Container(
+                                    //margin: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+                                    child: const Text("Speichern",
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(fontSize: 14))),
+                                style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.resolveWith<Color>(
                                     (Set<MaterialState> states) {
-                                  if (states.contains(MaterialState.pressed)) {
-                                    return Colors.greenAccent;
-                                  }
-                                  return Colors.green.shade900;
-                                },
+                                      if (states
+                                          .contains(MaterialState.pressed)) {
+                                        return Colors.greenAccent;
+                                      }
+                                      return Colors.green.shade900;
+                                    },
+                                  ),
+                                ),
                               ),
-                            ),
-                          ),
-                        )
-                      ]),
-                ),
-              );
-            }).then((value) {
-          setState(() {
-            if (dateChanged) {
-              widget.changeDate(widget.oldName, selectedDate);
-            }
-            if (nameChanged) {
-              widget.changeName(widget.oldName, value);
-            }
-            textController.clear();
-            dateChanged = false;
-            nameChanged = false;
-          });
-        });
-      }
-    },itemBuilder: (context) => [
-      const PopupMenuItem(child: Text("Eintrag ändern"),value: 0)
-    ]
-
-
-        );
-
+                            )
+                          ]),
+                    ),
+                  );
+                }).then((value) {
+              setState(() {
+                if (dateChanged) {
+                  widget.changeDate(widget.oldName, selectedDate);
+                }
+                if (nameChanged) {
+                  widget.changeName(widget.oldName, value);
+                }
+                textController.clear();
+                dateChanged = false;
+                nameChanged = false;
+              });
+            });
+          }
+        },
+        itemBuilder: (context) => [
+              const PopupMenuItem(child: Text("Eintrag ändern"), value: 0),
+              const PopupMenuItem(child: Text("Pflanze Gießen"), value: 1),
+              const PopupMenuItem(
+                  child: Text("In die Blüte schicken"), value: 2)
+            ]);
   }
 }
-
-//TODO Return only Modal but how, without icon
